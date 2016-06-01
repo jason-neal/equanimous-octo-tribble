8cf85792f4d6b22f4b44a7c124cee8da10bf3a3d
Jason Neal <jason.neal@astro.up.pt>
2016-06-01 19:20:37 +0100

    Air2vac vac2air conversions
    Should probably just use pyastronomy if need this.

--- SpectralTools.py
+++ SpectralTools.py
@@ -2,7 +2,7 @@
 
 
 # Collection of useful tools for dealing with spectra:
-
+from __future__ import division
 
 
 def BERVcorr(W1, Berv):
@@ -38,3 +38,41 @@ def inverse_BERV(W0, Berv):
 
 def Convolution():  # IP convolution ?
     return None
+
+
+
+def air2vac(air):
+	""" Conversion of air wavelenghts to vacuum wavelenghts.
+    Adapted from wcal from Pat Hall's IRAF tasks for displaying SDSS spectra
+
+    Input: Air wavelengths in nm
+
+    Output: Vacuum wavelengths in nm
+
+    """
+    print("Probably best to use pyastronomy versions !!!!!!!!!!!!!!!!!!!!!!!!")
+    air_in_ang = air * 10
+   
+    sigma2 = (10**8)/air**2
+    n = 1 + 0.000064328 + 0.0294981/(146-sigma2) + 0.0002554/(41 - sigma2)
+
+    vacuum_in_ang = air * n
+    if (min(air_in_ang)<1600)
+        print ("# WARNING! formula intended for use only at >1600 Ang!")
+
+    return vacuum_in_ang/10
+
+def vac2air(vac): 
+	""" Conversion of vacuum wavelenghts to air wavelenghts."""
+
+	"""From http://classic.sdss.org/dr7/products/spectra/vacwavelength.html
+	 AIR = VAC / (1.0 + 2.735182E-4 + 131.4182 / VAC^2 + 2.76249E8 / VAC^4) given in Morton (1991, ApJS, 77, 119)""""
+	 """ Better correction for infrared may be found in 
+	 http://adsabs.harvard.edu/abs/1966Metro...2...71E
+	and 
+	http://adsabs.harvard.edu/abs/1972JOSA...62..958P"""
+	print("Probably best to use pyastronomy versions !!!!!!!!!!!!!!!!!!!!!!!!")
+    vac_in_ang = vac * 10
+    air_in_ang = vac_in_ang / (1.0 + 2.735182E-4 + 131.4182 / vac_in_ang**2 + 2.76249E8 / vac_in_ang**4)
+    ##Need to look at these for nir compatbile formular
+	return air_in_ang/10
