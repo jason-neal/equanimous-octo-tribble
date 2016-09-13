f1e24b0b9f6ed7fe309dbe085bab920470cfb071
Jason Neal <jason.neal@astro.up.pt>
2016-09-13 11:25:05 +0100

    begin flux_ratio script

---
+++ flux_ratio.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+"""Brown Dwarf Flux ratio calculator:
+
+Inputs :
+Star name:
+Mass of companion in Jupiter masses
+Age of star: """
+
+from __future__ import division, print_function
+import numpy as np
+def main(star_name, companion_mass, stellar_age):
+    """Compute flux ratio of star to companion """
+    pass
