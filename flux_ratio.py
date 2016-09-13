6b5a9a14c98251bf4b8651401a88f5ece50068bb
Jason Neal <jason.neal@astro.up.pt>
2016-09-13 22:24:10 +0100

    Access baraffe data using np.loadtxt
    iinterpolate to mass of companion and return dict of the values

--- flux_ratio.py
+++ flux_ratio.py
@@ -8,7 +8,10 @@ Mass of companion in Jupiter masses
 Age of star: """
 
 from __future__ import division, print_function
+import argparse
 import numpy as np
+from astroquery.simbad import Simbad
+import pandas as pd
 
 def _parser():
     """Take care of all the argparse stuff.
@@ -29,7 +32,7 @@ def main(star_name, companion_mass, stellar_age):
     """Compute flux ratio of star to companion """
     
     # Obtain Stellar parameters from astroquery
-    star_params = get_stellar_params(star_name):
+    star_params = get_stellar_params(star_name)
     
     # Get parameters for this mass and age 
     companion = get_brown_dwarf_information(companion_mass, stellar_age)
@@ -37,7 +40,7 @@ def main(star_name, companion_mass, stellar_age):
     Flux_ratios = calculate_flux_ratios(star_params, companion)
 
     # Print flux ratios using a generator
-    print("{} band companion/star Flux ratio = {} ".format(key, val) for key, val in Flux_ratios.(dic unpack method))
+    print("{} band companion/star Flux ratio = {} ".format(key, val) for key, val in Flux_ratios.unpac())
 
     # Compare to area ratio
     Rstar = calculate_stellar_radius(star_params)
@@ -50,23 +53,28 @@ def calculate_flux_ratios(star_params, companion_params):
     """ Flux ratios for the different bands """
     f = 2.512
     Flux_ratios = dict()
-    Flux_ratios["J"] = f ** (companion_params["Jmag"]-star_params["Jmag"])
-    Flux_ratios["H"] = f ** (companion_params["Hmag"]-star_params["Hmag"])
-    Flux_ratios["K"] = f ** (companion_params["Kmag"]-star_params["Kmag"])
+    Flux_ratios["J"] = f ** (companion_params["Mj"]-star_params["Jmag"])
+    Flux_ratios["H"] = f ** (companion_params["Mh"]-star_params["Hmag"])
+    Flux_ratios["K"] = f ** (companion_params["Mk"]-star_params["Kmag"])
     return Flux_ratios
 
 def get_stellar_params(star_name):
     """ Astro query search """
 
     #return Magnitudes, parralax, Temp
-    pass
+    Simbad
+    columns = ["HD", "SpType", "B-V", "Vmag"]
+    result_table = Simbad.query_object(star_name, columns=columns)
+    print("Table colums", result_table.columns)
+    
+    
 
 
 def calculate_stellar_radius(star_params):
     """ Based on R/Rs = (Ts/T)^2(L/Ls)^(1/2) equation"""
     BminusV = star_params["Bmag"] - star_params["Vmag"]
 
-    if "Teff in star_params.keys(): # need table and interpolate to this B-V
+    if Teff in star_params.keys(): # need table and interpolate to this B-V
         teff_star
     else: #
         #Interpolate from B-V
@@ -87,13 +95,27 @@ def calculate_stellar_radius(star_params):
     pass
 
 
-def get_brown_dwarf_information(star_name, companion_mass, age):
+def get_brown_dwarf_information(companion_mass, age):
     """ baraffe 2003 table search
     Need the tables in a file somewhere"""
+
+    mass_solar = companion_mass / 1047.56   # covert to solar mass
+    BD_parameters = dict()
     
+    # Find closest age model
+    modelages = ["0.001", "0.005", "0.010", "0.050", "0.100", "0.120", "0.500", "1.000", "5.000", "10.000"]
+    model_age = min(modelages, key=lambda x:abs(float(x)-age)) # Closest one
+    model_id = "p".join(str(model_age).split("."))
 
+    model_name = "./data/Baraffe2003/BaraffeCOND2003-" +  model_id + "Gyr.dat"
+    model_data = np.loadtxt(model_name, skiprows=18, unpack=False)
+    model_data = model_data.T
 
-    print("Interpolated companion parameters from barraffe 2003 tables.")
+    cols = ["M/Ms", "Teff", "L/Ls", "g", "R", "Mv", "Mr", "Mi", "Mj", "Mh", "Mk", "Mll", "Mm"]
+    for col, data in zip(cols, model_data):
+        # Interpolate columns to mass of companion
+        BD_parameters[col] = np.interp(mass_solar, model_data[0], data)
+
     return  BD_parameters  # as a dictionary
 
 
