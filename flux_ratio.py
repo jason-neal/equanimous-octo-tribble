57902bafd7e5aacc74b804a321f120568a6381e0
Jason Neal <jason.neal@astro.up.pt>
2016-09-14 19:12:12 +0100

    Get temp function to try different sources.
    Rearrange function order

--- flux_ratio.py
+++ flux_ratio.py
@@ -40,7 +40,7 @@ def main(star_name, companion_mass, stellar_age):
     Flux_ratios = calculate_flux_ratios(star_params, companion)
 
     # Compare to area ratio
-    Rstar = calculate_stellar_radius(star_params)
+    Rstar = calculate_stellar_radius(star_name, star_params)
     Rcomp_Rstar = companion["R"] / Rstar
 
     # Print flux ratios using a generator
@@ -51,16 +51,10 @@ def main(star_name, companion_mass, stellar_age):
     print("Radius Ratio of companion/star    = {} ".format(Rcomp_Rstar))
     print("Area Ratio of companion/star      = {} ".format(Rcomp_Rstar**2))
     
-    get_sweet_cat_data()
 
-def calculate_flux_ratios(star_params, companion_params):
-    """ Flux ratios for the different bands """
-    f = 2.512
-    Flux_ratios = dict()
-    Flux_ratios["J"] = f ** (companion_params["Mj"]-star_params["FLUX_J"])
-    #Flux_ratios["H"] = f ** (companion_params["Mh"]-star_params["Hmag"])
-    Flux_ratios["K"] = f ** (companion_params["Mk"]-star_params["FLUX_K"])
-    return Flux_ratios
+##############################################################################
+# Access Databases
+##############################################################################
 
 
 def get_stellar_params(star_name):
@@ -77,38 +71,6 @@ def get_stellar_params(star_name):
     #print("Table colums", result_table.colnames)
     
     return result_table
-
-
-def calculate_stellar_radius(star_params):
-    """ Based on R/Rs = (Ts/T)^2(L/Ls)^(1/2) equation"""
-    BminusV = star_params["FLUX_B"] - star_params["FLUX_V"]
-    print(BminusV, "b-v")
-
-    #if "Fe_H_Teff" in star_params.keys(): # need table and interpolate to this B-V
-    #    teff_star = star_params["Fe_H_Teff"]
-    #    print(teff_star)
-    #    print(teff_star[0])
-    #    print("temperature fromsimbad =", teff_star)
-    #else:
-    #    teff_star = 0
-    if True:
-    #if teff_star == 0:
-        #Interpolate from B-V
-        bminusvs=[-0.31, -0.24, -0.20, -0.12, 0.0, 0.15, 0.29, 0.42, 0.58, 0.69, 0.85, 1.16, 1.42, 1.61] 
-        temps = [34000, 23000, 18500, 13000, 9500, 8500, 7300, 6600, 5900, 5600, 5100, 4200, 3700, 3000]
-        #teff_star = (4200-5100)/(1.16-0.85) * (BminusV-0.85) + 5100    # Linear interpolation
-        teff_star = np.interp(BminusV, bminusvs, temps)
-        print("Temperature of star from b-v", teff_star)
-
-    Ts_T = 5800. / teff_star              # Temperature ratio
-    Dm =   4.83 - star_params["FLUX_V"]   # Differnce of aboslute magnitude
-    L_Ls = 2.51 ** Dm                     # Luminosity ratio
-    R_Rs = (Ts_T)**2 * np.sqrt(L_Ls)      # Raidus of Star in Solar Radii
-
-    return R_Rs   # R star in solar radii
-    #BD_R = BD_Radius / R_Rs          # Radius_bd / Radius_star
-    #BD_area_ratio =  BD_R**2
-
 
 
 def get_brown_dwarf_information(companion_mass, age):
@@ -135,38 +97,92 @@ def get_brown_dwarf_information(companion_mass, age):
     return  BD_parameters  # as a dictionary
 
 
-def get_sweet_cat_data():
+def get_sweet_cat_temp(star_name):
     sc = pyasl.SWEETCat()
     data = sc.data
     print(data.head())
     print(data.keys())
-    #data.plot('teff', 'vmag', kind='scatter')
-    #plt.show()
 
-    # We can also create another column for our table, e.g. luminosity so we can make a HR diagram
-    # Note that this works even though we have missing values
-    data['lum'] = (data.teff/5777)**4 * data.mass
-    #data.plot('teff', 'lum', kind='scatter', xlim=(8500, 2500))
-
-    # If we only are interested in a shorter table, with stars with Teff below 5000K, we can do so
-    data_cool = data[data.teff < 5000]
-
-    # See that it worked (print rows and columns)
-    print(data.shape)
-    print(data_cool.shape)
-
-    # And the plot
-    #plt.figure()
-    #data.plot('teff', 'lum', kind='scatter', xlim=(8500, 2500))
-    #plt.plot(data_cool.teff, data_cool.lum, 'oy')
-    #plt.show()
-    
     # Assuming given as hd******
-    number = star_name[2:]
-    this_entry = data[data.hd == number]
-    print("this entry = ", this_entry)
-   # Need to check for empty frames if the star is not in sweet-cat
+    hd_number = star_name[2:]
+    star_entry = data[data.hd == hd_number]
+    print("star entry = ", star_entry)
+    #if star_entry is empty table: (star not found)
+    #    pass
+    #else:
+    star_temp = star_entry["teff"]
+    # Need to check for empty frames if the star is not in sweet-cat
+    return star_temp
 
+
+
+
+
+
+##############################################################################
+# Calculations
+##############################################################################
+def calculate_flux_ratios(star_params, companion_params):
+    """ Flux ratios for the different bands """
+    f = 2.512
+    Flux_ratios = dict()
+    Flux_ratios["J"] = f ** (companion_params["Mj"]-star_params["FLUX_J"])
+    #Flux_ratios["H"] = f ** (companion_params["Mh"]-star_params["Hmag"])
+    Flux_ratios["K"] = f ** (companion_params["Mk"]-star_params["FLUX_K"])
+    return Flux_ratios
+
+def get_temp_value(star_name):
+    """ Try get temperature of star multiple ways
+
+    1st - Try Fe_H_Teff param from Simbad.
+    2nd - Try SweetCat but the star might not be there
+    3rd - Calculate from B-V and interpolation"""
+    good_temp = False
+
+    if "Fe_H_Teff" in star_params.keys(): # need table and interpolate to this B-V
+        print(star_params["Fe_H_Teff"])
+        teff = star_params["Fe_H_Teff"][0]
+        if teff == 0 or teff == [0]:
+            # No teff given by Simbad 
+            teff = None
+        else:
+            good_temp = True
+
+    if not good_temp:
+        try:
+            teff = get_sweet_cat_temp(star_params, star_name)
+            if teff == 0 or teff == [0]:  # temp from seet-cat
+                teff = None
+            else:
+                good_temp = True
+        except:
+            good_temp = False
+
+    if not good_temp:
+        """Then use the B-V method as last resort"""
+        BminusV = star_params["FLUX_B"] - star_params["FLUX_V"]
+        print(BminusV, "b-v")
+        #Interpolate from B-V
+        bminusvs = [-0.31, -0.24, -0.20, -0.12, 0.0, 0.15, 0.29, 0.42, 0.58, 0.69, 0.85, 1.16, 1.42, 1.61] 
+        temps = [34000, 23000, 18500, 13000, 9500, 8500, 7300, 6600, 5900, 5600, 5100, 4200, 3700, 3000]
+        #teff_star = (4200-5100)/(1.16-0.85) * (BminusV-0.85) + 5100    # Linear interpolation
+        teff_star = np.interp(BminusV, bminusvs, temps)
+        print("Temperature of star from b-v", teff_star)
+
+    return teff_star
+
+
+def calculate_stellar_radius(star_name, star_params):
+    """ Based on R/Rs = (Ts/T)^2(L/Ls)^(1/2) equation"""
+
+    teff_star = get_temperature(star_name, star_params)
+
+    Ts_T = 5800. / teff_star              # Temperature ratio
+    Dm =   4.83 - star_params["FLUX_V"]   # Differnce of aboslute magnitude
+    L_Ls = 2.51 ** Dm                     # Luminosity ratio
+    R_Rs = (Ts_T)**2 * np.sqrt(L_Ls)      # Raidus of Star in Solar Radii
+
+    return R_Rs   # R star in solar radii
 
 
 if __name__ == '__main__':
