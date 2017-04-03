2032b3bbb9ae47454e10d7414d4325470037f429
Jason Neal <jason.neal@astro.up.pt>
2017-04-03 22:32:12 +0100

    s profile equation

---
+++ s_profile.py
@@ -0,0 +1,11 @@
+
+def s_profile(wave, wav_0, depth, width, k):
+    """Generate an S-profile from appendix of ferluga et al. """
+
+    s_amp = 2 * depth * np.exp(-np.pi * depth**2 * ((wav - wav_0)**2 + ()**2) / width**2)
+    s_inner = (np.pi * depth**2 * (wav - wav_0) * k) / width**2
+    s_lambda = s_amp * np.sinh(s_inner)
+    return s_lambda
+
+
+# I1, W1 and wav_1 are peak parameters.
