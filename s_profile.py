c2d5763d467caae7be08e40ef7cfc670e5c353d2
Jason Neal <jason.neal@astro.up.pt>
2017-04-18 13:12:05 +0100

    Plot s-profile.

--- s_profile.py
+++ s_profile.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from RVCalculations import Calculate_WLshift
 
 def s_profile(wave, wav_0, depth, width, k):
     """Generate an S-profile from appendix of ferluga et al. """
@@ -9,3 +12,28 @@ def s_profile(wave, wav_0, depth, width, k):
 
 
 # I1, W1 and wav_1 are peak parameters.
+
+wav_center = 2110.5
+depth = 0.5
+width = 1
+
+rv1 = Calculate_WLshift(wav_center, 500)
+rv2 = Calculate_WLshift(wav_center, 2000)
+rv3 = Calculate_WLshift(wav_center, 5000)
+rv4 = Calculate_WLshift(wav_center, 10000)
+
+lamda = np.linspace(2090, 2130, 50)
+
+s1 = s_profile(lamda, wav_center, depth, width, rv1)
+s2 = s_profile(lamda, wav_center, depth, width, rv2)
+s3 = s_profile(lamda, wav_center, depth, width, rv3)
+s4 = s_profile(lamda, wav_center, depth, width, rv4)
+
+plt.plot(lamda, s1, label="0.5 km/s")
+plt.plot(lamda, s2, label="2 km/s")
+plt.plot(lamda, s3, label="5 km/s")
+plt.plot(lamda, s4, label="10 km/s")
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Amplitude")
+plt.legend()
+plt.show()
