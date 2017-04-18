dfbedee3e7b2786328db542a85155a9e42583c1f
Jason Neal <jason.neal@astro.up.pt>
2017-04-18 13:11:49 +0100

    Fix s-profile equation

--- s_profile.py
+++ s_profile.py
@@ -2,8 +2,8 @@
 def s_profile(wave, wav_0, depth, width, k):
     """Generate an S-profile from appendix of ferluga et al. """
 
-    s_amp = 2 * depth * np.exp(-np.pi * depth**2 * ((wav - wav_0)**2 + ()**2) / width**2)
-    s_inner = (np.pi * depth**2 * (wav - wav_0) * k) / width**2
+    s_amp = 2 * depth * np.exp(-np.pi * depth**2 * ((wave - wav_0)**2 + (k/2)**2) / width**2)
+    s_inner = (np.pi * depth**2 * (wave - wav_0) * k) / width**2
     s_lambda = s_amp * np.sinh(s_inner)
     return s_lambda
 
