58e680095669b46031b4a9a2b441313df5180529
Jason Neal <jason.neal@astro.up.pt>
2017-05-02 16:18:09 +0100

    Test modification of tick labels.
    And add get gcd to Misc.

---
+++ Misc/tick_label_modification.py
@@ -0,0 +1,31 @@
+
+# Test modify xtick labels
+import matplotlib.pyplot as plt
+import numpy as np
+
+def convert_labels_to_rv(x, labels):
+    x_mid = np.mean(x)
+    c = 299792.458   # km/s
+    transform = ["{0:2.1f}".format((label / x_mid) * c) for label in labels]
+    return transform
+
+
+x = np.arange(10) + 2110
+y = (np.arange(10) - 2) * 0.01
+
+ax = plt.subplot(111)
+
+plt.plot(x, y)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+
+ax2 = ax.twinx()
+ax2.plot(x, y)
+ax2.set_ylabel("Transformed (km/s)")
+
+locations = [item for item in ax.get_yticks()]
+
+print(locations)
+new_labels = convert_labels_to_rv(x, locations)
+ax2.set_yticklabels(new_labels)
+plt.show()
